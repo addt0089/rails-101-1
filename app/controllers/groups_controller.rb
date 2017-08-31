2241b702c17f35db617c1c5e3c291a2c361a219a
Chen Wei Yu <enoch@Chens-MacBook-Pro.local>
2017-08-31 15:00:04 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,7 +15,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
-  def creat 
+  def create
     @group = Group.new(group_params)
     @group.save
     redirect_to groups_path
@@ -24,13 +24,9 @@ end
 
 def update
   @group = Group.find(params[:id])
-  if @group.update(group_params)
-    redirect_to groups_path, notice: "Update Success"
-  else
-    render :edit
-  end
+  @group.update(group_params)
+  redirect_to groups_path, notice: "Update Success"
 end
-
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
@@ -42,7 +38,4 @@ end
 
   def group_params
     params.require(:group).permit(:title, :description)
-  end
-
-end
-	
+  end	
